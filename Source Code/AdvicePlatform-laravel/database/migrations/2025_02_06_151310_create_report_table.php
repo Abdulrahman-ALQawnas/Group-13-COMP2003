2da6832bf14e7e29b7234d488ffeea58a27597e2
mahaalmas <maha.almasoudi@students.plymouth.ac.uk>
2025-02-06 15:15:57 +0000

    Create report table

---
+++ Source Code/AdvicePlatform-laravel/database/migrations/2025_02_06_151310_create_report_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->refernces('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->text('reason')->nullable();
+            $table->datetime('reported_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('report');
+    }
+}
